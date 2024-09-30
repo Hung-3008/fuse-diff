f375d4662daeb3837c3a68ea06d0aa5233c6c650
hungbv16 <hungbv16@fpt.com>
2024-10-01 00:08:30 +0700

    up

--- BraTS2020/light_training/trainer.py
+++ BraTS2020/light_training/trainer.py
@@ -1,3 +1,14 @@
+# Copyright 2020 - 2022 MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 from tqdm import tqdm
 import time
@@ -7,8 +18,11 @@ import torch.nn.parallel
 import torch.utils.data.distributed
 
 from monai.data import DataLoader
+
+
 import argparse
 from .launch import launch_dist
+
 from monai.utils import set_determinism
 from .sampler import SequentialDistributedSampler, distributed_concat
 from torch.utils.tensorboard import SummaryWriter
@@ -42,6 +56,7 @@ class Trainer:
         self.auto_optim = True
 
         torch.backends.cudnn.enabled = True
+        # torch.backends.cudnn.benchmark = True
 
         gpu_count = torch.cuda.device_count()
         if num_gpus > gpu_count:
@@ -62,55 +77,70 @@ class Trainer:
                 os._exit(1)
             self.initialize_distributed()
 
+
     def initialize_distributed(self):
         """Initialize torch.distributed."""
         if self.env_type == 'pytorch':
             self.print_rank_0('No need to initialize')
             return
         if self.env_type == 'DDP' or "deepspeed" in self.env_type:
+
             if self.local_rank is not None:
                 device = self.local_rank
             torch.cuda.set_device(device)
+            # Call the init process
             init_method = 'env://'
+            # self.master_ip = os.getenv('MASTER_ADDR', 'localhost')
+            # self.master_port = os.getenv('MASTER_PORT', '6000')
+            # init_method += self.master_ip + ':' + self.master_port
+            # print(init_method, self.rank, device, self.local_rank)
             torch.distributed.init_process_group(
                 backend='nccl',
+                # rank=self.rank,
                 init_method=init_method)
             self.world_size = torch.distributed.get_world_size()
+            # 模型初始化相同，但是训练时要保证数据增强不同。
+
             print(f"world size is {self.world_size}")
 
     def get_dataloader(self, dataset, shuffle=False, batch_size=1, train=True):
-        if dataset is None:
+        if dataset is None :
             return None
         if self.env_type == 'pytorch':
             return DataLoader(dataset,
-                              batch_size=batch_size,
-                              shuffle=shuffle,
-                              num_workers=12)
-        else:
+                                batch_size=batch_size,
+                                shuffle=shuffle,
+                                num_workers=12)
+        else :
             if not train:
                 sampler = SequentialDistributedSampler(dataset, batch_size=batch_size)
-            else:
+
+            else :
                 sampler = torch.utils.data.distributed.DistributedSampler(dataset, shuffle=True)
             return DataLoader(dataset,
-                              batch_size=batch_size,
-                              num_workers=12,
-                              sampler=sampler,
-                              drop_last=False)
+                                batch_size=batch_size,
+                                num_workers=12, 
+                                sampler=sampler, 
+                                drop_last=False)
 
     def get_dist_args(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument('--local_rank', type=int, default=0, help="local_rank")
+        parser.add_argument('--local_rank', type=int, default = 0, help="local_rank")
         parser.add_argument('--not_call_launch',
                             action='store_true',
                             help="not call launch!")
-        #ds_args = parser.parse_args()
-        ds_args, unknown = parser.parse_known_args()
-        self.rank = int(os.environ.get('RANK', 0))
+        ds_args = parser.parse_args()
+        self.rank = int(os.environ.get('RANK',0))
+        # self.local_rank = int(os.environ["LOCAL_RANK"])
+
         self.local_rank = ds_args.local_rank
         self.not_call_launch = ds_args.not_call_launch
         self.device = self.local_rank
+    
+        # self.master_addr = os.environ.get('MASTER_ADDR','127.0.0.1')
+        # self.master_port = os.environ.get('MASTER_PORT','17500')
 
-    def validation_single_gpu(self, val_dataset):
+    def validation_single_gpu(self, val_dataset,):
         if self.ddp:
             print(f"single gpu model not support the ddp")
             exit(0)
@@ -124,17 +154,19 @@ class Trainer:
                     x: batch[x].to(self.device)
                     for x in batch if isinstance(batch[x], torch.Tensor)
                 }
-            elif isinstance(batch, list):
+            elif isinstance(batch, list) :
                 batch = [x.to(self.device) for x in batch if isinstance(x, torch.Tensor)]
+
             elif isinstance(batch, torch.Tensor):
                 batch = batch.to(self.device)
-            else:
+            
+            else :
                 print("not support data type")
                 exit(0)
 
             with torch.no_grad():
                 val_out = self.validation_step(batch)
-                assert val_out is not None
+                assert val_out is not None 
 
             return_list = False
             val_outputs.append(val_out)
@@ -143,6 +175,7 @@ class Trainer:
 
         val_outputs = torch.tensor(val_outputs)
         if not return_list:
+            # 说明只有一个变量
             length = 0
             v_sum = 0.0
             for v in val_outputs:
@@ -152,9 +185,9 @@ class Trainer:
 
             if length == 0:
                 v_sum = 0
-            else:
-                v_sum = v_sum / length
-        else:
+            else :
+                v_sum = v_sum / length             
+        else :
             num_val = len(val_outputs[0])
             length = [0.0 for i in range(num_val)]
             v_sum = [0.0 for i in range(num_val)]
@@ -168,17 +201,19 @@ class Trainer:
             for i in range(num_val):
                 if length[i] == 0:
                     v_sum[i] = 0
-                else:
+                else :
                     v_sum[i] = v_sum[i] / length[i]
         return v_sum, val_outputs
 
     def train(self,
-              train_dataset,
-              optimizer=None,
-              model=None,
-              val_dataset=None,
-              scheduler=None,
+                train_dataset,
+                optimizer=None,
+                model=None,
+                val_dataset=None,
+                scheduler=None,
               ):
+       
+        
         if scheduler is not None:
             self.scheduler = scheduler
 
@@ -203,29 +238,35 @@ class Trainer:
                 self.writer = None
             if self.model is not None:
                 self.model.cuda(self.local_rank)
+                # self.model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(self.model)
                 self.model = torch.nn.parallel.DistributedDataParallel(self.model,
-                                                                       device_ids=[self.local_rank],
-                                                                       output_device=self.local_rank,
-                                                                       find_unused_parameters=True)
-        else:
+                                                                    device_ids=[self.local_rank],
+                                                                    output_device=self.local_rank,
+                                                                    find_unused_parameters=True)
+         
+        else :
             print("not support env_type")
             exit(0)
 
         train_loader = self.get_dataloader(train_dataset, shuffle=True, batch_size=self.batch_size)
         if val_dataset is not None:
             val_loader = self.get_dataloader(val_dataset, shuffle=False, batch_size=1, train=False)
-        else:
-            val_loader = None
-
+        else :
+            val_loader = None 
+            
         for epoch in range(0, self.max_epochs):
-            self.epoch = epoch
+            self.epoch = epoch 
             if self.ddp:
                 train_loader.sampler.set_epoch(epoch)
                 torch.distributed.barrier()
-            self.train_epoch(train_loader, epoch)
-
+            self.train_epoch(
+                            train_loader,
+                            epoch,
+                            )
+            
             val_outputs = []
-            if (epoch + 1) % self.val_every == 0 and val_loader is not None:
+            if (epoch+1) % self.val_every == 0 \
+                    and val_loader is not None :
                 if self.model is not None:
                     self.model.eval()
                 if self.ddp:
@@ -236,32 +277,40 @@ class Trainer:
                             x: batch[x].to(self.device)
                             for x in batch if isinstance(batch[x], torch.Tensor)
                         }
-                    elif isinstance(batch, list):
+                    elif isinstance(batch, list) :
                         batch = [x.to(self.device) for x in batch if isinstance(x, torch.Tensor)]
+
                     elif isinstance(batch, torch.Tensor):
                         batch = batch.to(self.device)
-                    else:
+                    
+                    else :
                         print("not support data type")
                         exit(0)
 
                     with torch.no_grad():
                         val_out = self.validation_step(batch)
-                        assert val_out is not None
+                        assert val_out is not None 
 
                     return_list = False
                     val_outputs.append(val_out)
                     if isinstance(val_out, list) or isinstance(val_out, tuple):
                         return_list = True
 
+                ## 先汇总结果。
                 if self.ddp:
                     val_outputs = torch.tensor(val_outputs).cuda(self.local_rank)
                     torch.distributed.barrier()
+                    # gather_val_outputs = [torch.zeros_like(val_outputs) for _ in range(self.world_size)]
+                    # torch.distributed.all_gather(gather_val_outputs, val_outputs)
+                    # val_outputs = torch.cat(gather_val_outputs, dim=0)
+                    
                     val_outputs = distributed_concat(val_outputs, num_total_examples=len(val_loader.sampler.dataset))
-                else:
+                else :
                     val_outputs = torch.tensor(val_outputs)
 
                 if self.local_rank == 0:
                     if not return_list:
+                        # 说明只有一个变量
                         length = 0
                         v_sum = 0.0
                         for v in val_outputs:
@@ -271,10 +320,11 @@ class Trainer:
 
                         if length == 0:
                             v_sum = 0
-                        else:
-                            v_sum = v_sum / length
+                        else :
+                            v_sum = v_sum / length 
                         self.validation_end(mean_val_outputs=v_sum)
-                    else:
+                    
+                    else :
                         num_val = len(val_outputs[0])
                         length = [0.0 for i in range(num_val)]
                         v_sum = [0.0 for i in range(num_val)]
@@ -288,7 +338,7 @@ class Trainer:
                         for i in range(num_val):
                             if length[i] == 0:
                                 v_sum[i] = 0
-                            else:
+                            else :
                                 v_sum[i] = v_sum[i] / length[i]
 
                         self.validation_end(mean_val_outputs=v_sum)
@@ -298,11 +348,16 @@ class Trainer:
             if self.model is not None:
                 self.model.train()
 
-    def train_epoch(self, loader, epoch):
+
+    def train_epoch(self, 
+                    loader,
+                    epoch,
+                    ):
         if self.model is not None:
             self.model.train()
         if self.local_rank == 0:
             with tqdm(total=len(loader)) as t:
+
                 for idx, batch in enumerate(loader):
                     self.global_step += 1
                     t.set_description('Epoch %i' % epoch)
@@ -311,78 +366,83 @@ class Trainer:
                             x: batch[x].contiguous().to(self.device)
                             for x in batch if isinstance(batch[x], torch.Tensor)
                         }
-                    elif isinstance(batch, list):
+                    elif isinstance(batch, list) :
                         batch = [x.to(self.device) for x in batch if isinstance(x, torch.Tensor)]
+
                     elif isinstance(batch, torch.Tensor):
                         batch = batch.to(self.device)
-                    else:
+                    
+                    else :
                         print("not support data type")
                         exit(0)
-
+                    
                     if self.model is not None:
-                        for param in self.model.parameters():
-                            param.grad = None
+                        for param in self.model.parameters(): param.grad = None
                     loss = self.training_step(batch)
 
                     if self.auto_optim:
                         loss.backward()
                         self.optimizer.step()
-
+                        
                         lr = self.optimizer.state_dict()['param_groups'][0]['lr']
+
                         t.set_postfix(loss=loss.item(), lr=lr)
                     t.update(1)
-        else:
+        else :
             for idx, batch in enumerate(loader):
                 self.global_step += 1
                 if isinstance(batch, dict):
-                    batch = {
-                        x: batch[x].contiguous().to(self.device)
-                        for x in batch if isinstance(batch[x], torch.Tensor)
-                    }
-                elif isinstance(batch, list):
+                        batch = {
+                            x: batch[x].contiguous().to(self.device)
+                            for x in batch if isinstance(batch[x], torch.Tensor)
+                        }
+                elif isinstance(batch, list) :
                     batch = [x.to(self.device) for x in batch if isinstance(x, torch.Tensor)]
+
                 elif isinstance(batch, torch.Tensor):
                     batch = batch.to(self.device)
-                else:
+                
+                else :
                     print("not support data type")
-                    exit(0)
+                    exit(0)   
 
-                for param in self.model.parameters():
-                    param.grad = None
+                for param in self.model.parameters(): param.grad = None
 
                 loss = self.training_step(batch)
                 if self.auto_optim:
                     loss.backward()
                     self.optimizer.step()
 
-            for param in self.model.parameters():
-                param.grad = None
+            for param in self.model.parameters() : param.grad = None
 
     def training_step(self, batch):
         raise NotImplementedError
-
+    
     def validation_step(self, batch):
         raise NotImplementedError
 
     def validation_end(self, mean_val_outputs):
-        pass
+        pass 
+
 
     def log(self, k, v, step):
         if self.env_type == "pytorch":
             self.writer.add_scalar(k, scalar_value=v, global_step=step)
-        else:
+
+        else :
             if self.local_rank == 0:
                 self.writer.add_scalar(k, scalar_value=v, global_step=step)
 
     def load_state_dict(self, weight_path, strict=True):
         sd = torch.load(weight_path, map_location="cpu")
-        if "module" in sd:
+        if "module" in sd :
             sd = sd["module"]
         new_sd = {}
         for k, v in sd.items():
             k = str(k)
-            new_k = k[7:] if k.startswith("module") else k
-            new_sd[new_k] = v
+            new_k = k[7:] if k.startswith("module") else k 
+            new_sd[new_k] = v 
 
         self.model.load_state_dict(new_sd, strict=strict)
+        
         print(f"model parameters are loaded successed.")
--- BraTS2020/train.py
+++ BraTS2020/train.py
@@ -13,7 +13,7 @@ from unet.basic_unet import BasicUNetEncoder
 import argparse
 from monai.losses.dice import DiceLoss
 import yaml
-from guided_diffusion.gaussian_diffusion import get_named_beta_schedule, ModelMeanType, ModelVarType, LossType
+from guided_diffusion.gaussian_diffusion import get_named_beta_schedule, ModelMeanType, ModelVarType,LossType
 from guided_diffusion.respace import SpacedDiffusion, space_timesteps
 from guided_diffusion.resample import UniformSampler
 import os
@@ -29,31 +29,38 @@ class DiffUNet(nn.Module):
         self.embed_model = BasicUNetEncoder(3, number_modality, number_targets, [64, 64, 128, 256, 512, 64])
 
         self.model = BasicUNetDe(3, number_targets, number_targets, [64, 64, 128, 256, 512, 64], 
-                                act=("LeakyReLU", {"negative_slope": 0.1, "inplace": False}))
-    
+                                act = ("LeakyReLU", {"negative_slope": 0.1, "inplace": False}))
+   
         betas = get_named_beta_schedule("linear", 1000)
         self.diffusion = SpacedDiffusion(use_timesteps=space_timesteps(1000, [1000]),
-                                         betas=betas,
-                                         model_mean_type=ModelMeanType.START_X,
-                                         model_var_type=ModelVarType.FIXED_LARGE,
-                                         loss_type=LossType.MSE)
+                                            betas=betas,
+                                            model_mean_type=ModelMeanType.START_X,
+                                            model_var_type=ModelVarType.FIXED_LARGE,
+                                            loss_type=LossType.MSE,
+                                            )
+
         self.sample_diffusion = SpacedDiffusion(use_timesteps=space_timesteps(1000, [50]),
-                                                betas=betas,
-                                                model_mean_type=ModelMeanType.START_X,
-                                                model_var_type=ModelVarType.FIXED_LARGE,
-                                                loss_type=LossType.MSE)
+                                            betas=betas,
+                                            model_mean_type=ModelMeanType.START_X,
+                                            model_var_type=ModelVarType.FIXED_LARGE,
+                                            loss_type=LossType.MSE,
+                                            )
         self.sampler = UniformSampler(1000)
+
 
     def forward(self, image=None, x=None, pred_type=None, step=None):
         if pred_type == "q_sample":
             noise = torch.randn_like(x).to(x.device)
             t, weight = self.sampler.sample(x.shape[0], x.device)
             return self.diffusion.q_sample(x, t, noise=noise), t, noise
+
         elif pred_type == "denoise":
             embeddings = self.embed_model(image)
             return self.model(x, t=step, image=image, embeddings=embeddings)
+
         elif pred_type == "ddim_sample":
             embeddings = self.embed_model(image)
+
             sample_out = self.sample_diffusion.ddim_sample_loop(self.model, (1, number_targets, 96, 96, 96), model_kwargs={"image": image, "embeddings": embeddings})
             sample_out = sample_out["pred_xstart"]
             return sample_out
@@ -61,64 +68,95 @@ class DiffUNet(nn.Module):
 class BraTSTrainer(Trainer):
     def __init__(self, env_type, max_epochs, batch_size, device="cpu", val_every=1, num_gpus=1, logdir="./logs/", master_ip='localhost', master_port=17750, training_script="train.py"):
         super().__init__(env_type, max_epochs, batch_size, device, val_every, num_gpus, logdir, master_ip, master_port, training_script)
-        self.window_infer = SlidingWindowInferer(roi_size=[96, 96, 96], sw_batch_size=1, overlap=0.25)
+        self.window_infer = SlidingWindowInferer(roi_size=[96, 96, 96],
+                                        sw_batch_size=1,
+                                        overlap=0.25)
         self.model = DiffUNet(number_modality=4, number_targets=3)
+
         self.best_mean_dice = 0.0
         self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=1e-4, weight_decay=1e-3)
         self.ce = nn.CrossEntropyLoss() 
         self.mse = nn.MSELoss()
-        self.scheduler = LinearWarmupCosineAnnealingLR(self.optimizer, warmup_epochs=30, max_epochs=max_epochs)
+        self.scheduler = LinearWarmupCosineAnnealingLR(self.optimizer,
+                                                  warmup_epochs=30,
+                                                  max_epochs=max_epochs)
+
         self.bce = nn.BCEWithLogitsLoss()
         self.dice_loss = DiceLoss(sigmoid=True)
 
     def training_step(self, batch):
         image, label = self.get_input(batch)
         x_start = label
+
         x_start = (x_start) * 2 - 1
         x_t, t, noise = self.model(x=x_start, pred_type="q_sample")
         pred_xstart = self.model(x=x_t, step=t, image=image, pred_type="denoise")
+
         loss_dice = self.dice_loss(pred_xstart, label)
         loss_bce = self.bce(pred_xstart, label)
+
         pred_xstart = torch.sigmoid(pred_xstart)
         loss_mse = self.mse(pred_xstart, label)
+
         loss = loss_dice + loss_bce + loss_mse
+
         self.log("train_loss", loss, step=self.global_step)
+
         return loss 
-    
+ 
     def get_input(self, batch):
         image = batch["image"]
         label = batch["label"]
+       
         label = label.float()
         return image, label 
 
     def validation_step(self, batch):
         image, label = self.get_input(batch)    
+        
         output = self.window_infer(image, self.model, pred_type="ddim_sample")
+
         output = torch.sigmoid(output)
+
         output = (output > 0.5).float().cpu().numpy()
+
         target = label.cpu().numpy()
         o = output[:, 1]
-        t = target[:, 1]
+        t = target[:, 1] # ce
         wt = dice(o, t)
+        # core
         o = output[:, 0]
         t = target[:, 0]
         tc = dice(o, t)
+        # active
         o = output[:, 2]
         t = target[:, 2]
         et = dice(o, t)
+        
         return [wt, tc, et]
 
     def validation_end(self, mean_val_outputs):
         wt, tc, et = mean_val_outputs
+
         self.log("wt", wt, step=self.epoch)
         self.log("tc", tc, step=self.epoch)
         self.log("et", et, step=self.epoch)
+
         self.log("mean_dice", (wt+tc+et)/3, step=self.epoch)
+
         mean_dice = (wt + tc + et) / 3
         if mean_dice > self.best_mean_dice:
             self.best_mean_dice = mean_dice
-            save_new_model_and_delete_last(self.model, os.path.join(model_save_path, f"best_model_{mean_dice:.4f}.pt"), delete_symbol="best_model")
-        save_new_model_and_delete_last(self.model, os.path.join(model_save_path, f"final_model_{mean_dice:.4f}.pt"), delete_symbol="final_model")
+            save_new_model_and_delete_last(self.model, 
+                                            os.path.join(model_save_path, 
+                                            f"best_model_{mean_dice:.4f}.pt"), 
+                                            delete_symbol="best_model")
+
+        save_new_model_and_delete_last(self.model, 
+                                        os.path.join(model_save_path, 
+                                        f"final_model_{mean_dice:.4f}.pt"), 
+                                        delete_symbol="final_model")
+
         print(f"wt is {wt}, tc is {tc}, et is {et}, mean_dice is {mean_dice}")
 
 if __name__ == "__main__":
@@ -131,10 +169,8 @@ if __name__ == "__main__":
     parser.add_argument("--num_gpus", type=int, default=1, help="Number of GPUs to use")
     parser.add_argument("--device", type=str, default="cuda:0", help="Device to use for training")
     parser.add_argument("--env", type=str, default="pytorch", help="Environment type")
-    # parser.add_argument("--local_rank", type=int, default=0, help="Local rank for distributed training")
-    # parser.add_argument("--not_call_launch", action='store_true', help="Do not call launch function inside the script")
 
-    args, unknown = parser.parse_known_args()
+    args = parser.parse_args()
 
     model_save_path = os.path.join(args.logdir, "model")
 
